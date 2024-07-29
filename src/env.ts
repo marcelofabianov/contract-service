d52330fbaaa408cfe12e5d7bb95b57a58dff4ead
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-29 06:04:06 -0300

    chore: iniciar load env

---
+++ src/env.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+import { ErrorHandle } from './error'
+
+const envSchema = z.object({
+  NODE_ENV: z.string().default('development'),
+  CO_DB_URL: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  throw new ErrorHandle('Environment variables are not set correctly')
+}
+
+export const env = _env.data

39b0984379ae003e47bdf67e3451574d6184eb20
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-29 07:59:01 -0300

    chore: atualizar env

--- src/env.ts
+++ src/env.ts
@@ -2,14 +2,23 @@ import { z } from 'zod'
 import { ErrorHandle } from './error'
 
 const envSchema = z.object({
-  NODE_ENV: z.string().default('development'),
-  DATABASE_URL: z.string(),
+  CO_DB_HOST: z.string(),
+  CO_DB_PORT: z.string().transform(Number),
+  CO_DB_USER: z.string(),
+  CO_DB_PASSWORD: z.string(),
+  CO_DB_DATABASE: z.string(),
+  CO_DB_MODE: z.string(),
+  CO_DB_TIMEOUT: z.string().transform(Number),
+  CO_DB_IDLE_TIMEOUT: z.string().transform(Number),
+  CO_DB_MAX_CONNECTIONS: z.string().transform(Number),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  throw new ErrorHandle('Environment variables are not set correctly')
+  throw new ErrorHandle(
+    'Environment variables are not set correctly: ' + _env.error.format(),
+  )
 }
 
 export const env = _env.data
